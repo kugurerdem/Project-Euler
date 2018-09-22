211436af4206a30d1f75e562a5ad167d8bd4e3cd
Uğur Erdem <43253140+kugurerdem@users.noreply.github.com>
2018-09-22 23:21:22 +0300

    Add files via upload

--- 0009 - Special Pythagorean triplet/0009 - Brute Force Approach.py
+++ 0009 - Special Pythagorean triplet/0009 - Brute Force Approach.py
@@ -2,7 +2,7 @@ summation = 1000
 
 def productOfTriplets(summation):
     
-    for a in range(int(summation/2)):
+    for a in range(int(summation/3)):
         for b in range(int(summation/2)):
             c = 1000-a-b
             if( a**2 + b**2 == c**2):
